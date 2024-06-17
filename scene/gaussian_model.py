77276081e8201bf71c7a041ce4ffb1cc9ba42b82
Vadim Pryadilshchikov <vadim-pr2001@mail.ru>
2024-06-17 14:49:24 +0300

    fixup! fix dtype

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -253,8 +253,8 @@ class GaussianModel:
 
     def create_from_pcd(self, pcd : BasicPointCloud, spatial_lr_scale : float):
         self.spatial_lr_scale = spatial_lr_scale
-        fused_point_cloud = torch.tensor(np.asarray(pcd.points)).float().cuda()
-        fused_color = RGB2SH(torch.tensor(np.asarray(pcd.colors)).float().cuda())
+        fused_point_cloud = torch.tensor(np.asarray(pcd.points), dtype=torch.float32).float().cuda()
+        fused_color = RGB2SH(torch.tensor(np.asarray(pcd.colors), dtype=torch.float32).float().cuda())
         features = torch.zeros((fused_color.shape[0], 3, self.get_max_sh_channels)).float().cuda()
         features[:, :3, 0 ] = fused_color
         features[:, 3:, 1:] = 0.0
